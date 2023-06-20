65e24bb74cced15e2faefd988b3c9ba3a3c8994d
NorVirae <norbertmbafrank@gmail.com>
2023-06-20 13:19:56 +0100

    run backgrhound threader

--- threads-playground/Program.cs
+++ threads-playground/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Threading;
+
+
+public class LetsThreadIt
+{
+    public static void ThreadProcess()
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("ThreadProcL {0}, ThreadProc {1}", i, i+1);
+
+            Thread.Sleep(1000);
+
+        }
+    }
+
+
+    public static void Main()
+    {
+        int actionCount = 0;
+        bool continueRunning = true;
+        Thread runThread = new Thread(new ThreadStart(ThreadProcess));
+
+        runThread.Start();
+
+
+        while (continueRunning)
+        {
+            Console.WriteLine("Action count: {0}", actionCount);
+
+            actionCount++;
+            Thread.Sleep(1000);
+            if(actionCount > 10)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Thread is about to be Terminated");
+        runThread.Join();
+        continueRunning  = false;
+        Console.WriteLine("Thread Terminated");
+
+    }
+
+}
